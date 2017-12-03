711b3f2f2ce3a8ab599291217a0aee348a62c830
XJINE <xjine.andorelseif@gmail.com>
2017-12-04 04:52:41 +0900

    Add comment outed Vector2ArrayGUI & VectorArrayGUI.

--- Assets/XJGUI/ValueGUI.cs
+++ Assets/XJGUI/ValueGUI.cs
@@ -13,19 +13,5 @@
         public virtual bool WithSlider { get; set; }
 
         #endregion Property
-
-        #region Constructor
-
-        public ValueGUI() : base()
-        {
-            // NOTE:
-            // It seems good, but have a problem.
-            // Vector2 or Vector3 and any other Unity "value" has no "MinValue" or "MaxValue".
-            // 
-            // this.MinValue = (T)(typeof(T).GetField("MinValue").GetValue(null));
-            // this.MaxValue = (T)(typeof(T).GetField("MinValue").GetValue(null));
-        }
-
-        #endregion Constructor
     }
 }
---
+++ Assets/XJGUI/VectorArrayGUI.cs
@@ -0,0 +1,122 @@
+﻿//namespace XJGUI
+//{
+//    public abstract class VectorArrayGUI<T> : AbstractGUI<T>
+//    {
+//        #region Field
+
+//        #endregion Field
+
+//        #region Property
+
+//        public virtual T MinValue
+//        {
+//            get
+//            {
+//                return this.vector2GUIs[0].MinValue;
+//            }
+//            set
+//            {
+//                int length = this.vector2GUIs.Length;
+
+//                for (int i = 0; i < length; i++)
+//                {
+//                    this.vector2GUIs[i].MinValue = value;
+//                }
+//            }
+//        }
+
+//        public virtual T MaxValue
+//        {
+//            get
+//            {
+//                return this.vector2GUIs[0].MaxValue;
+//            }
+//            set
+//            {
+//                int length = this.vector2GUIs.Length;
+
+//                for (int i = 0; i < length; i++)
+//                {
+//                    this.vector2GUIs[i].MaxValue = value;
+//                }
+//            }
+//        }
+
+//        public int DecimalPlaces
+//        {
+//            get
+//            {
+//                return this.vector2GUIs[0].DecimalPlaces;
+//            }
+//            set
+//            {
+//                int length = this.vector2GUIs.Length;
+
+//                for (int i = 0; i < length; i++)
+//                {
+//                    this.vector2GUIs[i].DecimalPlaces = value;
+//                }
+//            }
+//        }
+
+//        public float TextFieldWidth
+//        {
+//            get
+//            {
+//                return this.vector2GUIs[0].TextFieldWidth;
+//            }
+//            set
+//            {
+//                int length = this.vector2GUIs.Length;
+
+//                for (int i = 0; i < length; i++)
+//                {
+//                    this.vector2GUIs[i].TextFieldWidth = value;
+//                }
+//            }
+//        }
+
+//        public bool WithSlider
+//        {
+//            get
+//            {
+//                return this.vector2GUIs[0].WithSlider;
+//            }
+//            set
+//            {
+//                int length = this.vector2GUIs.Length;
+
+//                for (int i = 0; i < length; i++)
+//                {
+//                    this.vector2GUIs[i].WithSlider = value;
+//                }
+//            }
+//        }
+
+//        public new T[] Value
+//        {
+//            get
+//            {
+//                return this.values;
+//            }
+//            set
+//            {
+//                int valuesLength = this.values.Length;
+
+//                if (valuesLength != value.Length)
+//                {
+//                    throw new System.Exception
+//                    (string.Format(ErrorMessage.DifferentDataLengthError, "Current Values", "New Values"));
+//                }
+
+//                for (int i = 0; i < valuesLength; i++)
+//                {
+//                    this.values[i] = value[i];
+//                    this.vector2GUIs[i].Value = value[i];
+//                }
+//            }
+//        }
+
+//        #endregion Property
+//    }
+//}
