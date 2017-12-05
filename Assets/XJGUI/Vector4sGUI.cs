548e1bfca089b1d1d79b371484e3abe7a7d69fde
umeda <umeda@team-lab.com>
2017-12-05 14:37:16 +0900

    Add Vector4sGUI.

---
+++ Assets/XJGUI/Vector4sGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace XJGUI
+{
+    public class Vector4sGUI : ValuesGUI<Vector4>
+    {
+        #region Method
+
+        protected override ValueGUI<Vector4> GenerateValueGUI()
+        {
+            return new Vector4GUI()
+            {
+                MinValue = base.minValue,
+                MaxValue = base.maxValue,
+                TextFieldWidth = base.textFieldWidth,
+                WithSlider = base.withSlider
+            };
+        }
+
+        #endregion Method
+    }
+}
