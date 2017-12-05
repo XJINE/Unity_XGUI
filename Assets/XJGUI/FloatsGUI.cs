b127e0b3caac0f63e1be08c72ad1f1777687d5b5
XJINE <xjine.andorelseif@gmail.com>
2017-12-05 11:13:45 +0900

    Add FloatsGUI.

---
+++ Assets/XJGUI/FloatsGUI.cs
@@ -0,0 +1,20 @@
+﻿namespace XJGUI
+{
+    public class FloatsGUI : ValuesGUI<float>
+    {
+        #region Method
+
+        protected override ValueGUI<float> GenerateValueGUI()
+        {
+            return new FloatGUI()
+            {
+                MinValue = base.minValue,
+                MaxValue = base.maxValue,
+                TextFieldWidth = base.textFieldWidth,
+                WithSlider = base.withSlider
+            };
+        }
+
+        #endregion Method
+    }
+}
