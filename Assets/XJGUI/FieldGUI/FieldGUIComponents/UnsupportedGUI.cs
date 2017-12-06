fd9fadaf8fe2c56fb652c331c3b4a46460180c29
umeda <umeda@team-lab.com>
2017-12-06 18:18:25 +0900

    Add FieldGUIComponents.UnsupportedGUI.

---
+++ Assets/XJGUI/FieldGUI/FieldGUIComponents/UnsupportedGUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace XJGUI
+{
+    public class UnSupportedGUI : FieldGUIBase
+    {
+        #region Constructor
+
+        public UnSupportedGUI(System.Object data, FieldInfo fieldInfo, FieldGUIInfo guiInfo)
+            :base(data, fieldInfo, guiInfo)
+        {
+        }
+
+        #endregion Constructor
+
+        #region Method
+
+        protected override void InitializeGUI()
+        {
+            // Nothing to do.
+        }
+
+        protected override void Save()
+        {
+            // Nothing to do.
+        }
+
+        protected override void Load()
+        {
+            // Nothing to do.
+        }
+
+        protected override void ShowGUI()
+        {
+            GUILayout.Label("Unsupported Field : " + base.fieldInfo.Name);
+        }
+
+        #endregion Method
+    }
+}
