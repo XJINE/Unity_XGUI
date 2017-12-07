e90cc74dd020fe3ac433b9cc21f3cbd01b335a35
XJINE <xjine.andorelseif@gmail.com>
2017-12-08 08:06:07 +0900

    Update TabPanel Skin.

--- Assets/XJGUI/TabPanel.cs
+++ Assets/XJGUI/TabPanel.cs
@@ -5,6 +5,12 @@ namespace XJGUI
 {
     public class TabPanel : PanelGUI<int>
     {
+        #region Field
+
+        private static GUIStyle ButtonStyle;
+
+        #endregion Field
+
         #region Property
 
         public string[] Labels { get; set; }
@@ -15,6 +21,20 @@ namespace XJGUI
 
         public override int Show(params Action[] guiAction)
         {
+            if (TabPanel.ButtonStyle == null)
+            {
+                TabPanel.ButtonStyle = new GUIStyle(GUI.skin.button);
+
+                if (GUI.skin.label.normal.background == null)
+                {
+                    TabPanel.ButtonStyle.onNormal.background = XJGUILayout.TransparentTexture;
+                }
+                else
+                {
+                    TabPanel.ButtonStyle.onNormal.background = GUI.skin.label.normal.background;
+                }
+            }
+
             //if (functions.Length != this.labels.Length)
             //{
             //    throw new Exception(string.Format(ErrorMessage.DifferentDataLengthError, functions, this.labels));
@@ -24,7 +44,7 @@ namespace XJGUI
             {
                 base.ShowTitle();
 
-                base.Value = GUILayout.Toolbar(base.Value, this.Labels);
+                base.Value = GUILayout.Toolbar(base.Value, this.Labels, TabPanel.ButtonStyle);
 
                 guiAction[base.Value]();
             }));
--- Assets/XJGUI/XJGUILayout.cs
+++ Assets/XJGUI/XJGUILayout.cs
@@ -3,6 +3,22 @@ using UnityEngine;
 
 public static class XJGUILayout
 {
+    #region Field
+
+    public static Texture2D TransparentTexture;
+
+    #endregion Field
+
+    #region Constructor
+
+    static XJGUILayout()
+    {
+        XJGUILayout.TransparentTexture = GenerateBackgroundTexture(Color.clear);
+        XJGUILayout.TransparentTexture.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    #endregion Constructor
+
     #region Method
 
     public static void HorizontalLayout(Action guiAction)
@@ -21,6 +37,25 @@ public static class XJGUILayout
             guiAction();
         }
         GUILayout.EndVertical();
+    }
+
+    private static Texture2D GenerateBackgroundTexture(Color color)
+    {
+        int width  = 1;
+        int height = 1;
+
+        Color[] pixels = new Color[width * height];
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = color;
+        }
+
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(pixels);
+        texture.Apply();
+
+        return texture;
     }
 
     #endregion Method
