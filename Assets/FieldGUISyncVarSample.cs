41b0be2dd46e9c48768e3b7dce87e3b1495cd384
XJINE <xjine.andorelseif@gmail.com>
2017-12-09 16:29:49 +0900

    Add Packages/CustomNetworkManager & Add FieldGUISyncVarSample, but This is Useless.

--- Assets/FieldGUISyncVarSample.cs
+++ Assets/FieldGUISyncVarSample.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using XJGUI;
 
-public class FieldGUISample : MonoBehaviour
+public class FieldGUISyncVarSample : NetworkBehaviour
 {
     #region Field
 
@@ -11,35 +11,48 @@ public class FieldGUISample : MonoBehaviour
 
     public FieldGUI fieldGUI;
 
+    [SyncVar]
     [FieldGUIInfo(Title = "Hide UnsupportedGUI")]
     public bool boolValue = false;
 
+    [SyncVar]
     public string stringValue = "Sample";
 
+    [SyncVar]
     [FieldGUIInfo(MinValue = 0, MaxValue = 100)]
     public int intValue = 0;
 
+    [SyncVar]
     [FieldGUIInfo(HideInGUI = true)]
     public int intValueHide = 5;
 
+    [SyncVar]
     public float floatValue = 10;
 
+    [SyncVar]
     public Vector2 vector2Value = new Vector2(0, 0);
 
+    [SyncVar]
     public Vector3 vector3Value = new Vector3(1, 1, 1);
 
+    [SyncVar]
     public Vector4 vector4Value = new Vector4(1, 1, 1, 1);
 
+    //[SyncVar]
     public List<int> intValues = new List<int>() { 0, 1, 2 };
 
+    //[SyncVar]
     [FieldGUIInfo(Decimals = 1)]
     public float[] floatValues = new float[] { 0, 1, 2 };
 
+    //[SyncVar]
     public List<Vector3> vector3Values = new List<Vector3>() { Vector3.one, Vector3.up, Vector3.back };
 
+    [SyncVar]
     [FieldGUIInfo(Title = "Camera Clear Enum")]
     public CameraClearFlags enumValue = CameraClearFlags.Skybox;
 
+    [SyncVar]
     [FieldGUIInfo(IPv4 = true)]
     public string ipV4Value = "0.7.7.7";
 
