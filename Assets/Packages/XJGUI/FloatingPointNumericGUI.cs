5c682531d5147266cb135fe2664ae3807af4ba0d
XJINE <xjine.andorelseif@gmail.com>
2019-02-19 14:33:39 +0900

    Add FloatingPointNumericGUI.

---
+++ Assets/Packages/XJGUI/FloatingPointNumericGUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace XJGUI
+{
+    public abstract class FloatingPointNumericGUI<T> : NumericGUI<T> where T : struct, IComparable
+    {
+        #region Property
+
+        public virtual int Decimals { get; set; }
+
+        #endregion Property
+
+        #region Constructor
+
+        public FloatingPointNumericGUI() : base()
+        {
+            this.Decimals = XJGUILayout.DefaultDecimals;
+        }
+
+        #endregion Constructor
+    }
+}
