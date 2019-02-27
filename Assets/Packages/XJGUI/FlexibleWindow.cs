c410ef815a6ac9a525c6b0e5a922aa143c0d811c
XJINE <xjine.andorelseif@gmail.com>
2019-02-27 18:26:21 +0900

    Temp commit.

--- Assets/Packages/XJGUI/FlexibleWindow.cs
+++ Assets/Packages/XJGUI/FlexibleWindow.cs
@@ -66,7 +66,7 @@ namespace XJGUI
                 {
                     guiAction[i]();
                 }
-                
+
                 if (this.IsDraggable)
                 {
                     GUI.DragWindow();
