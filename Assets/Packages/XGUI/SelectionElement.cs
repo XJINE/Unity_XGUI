8a9f976d4e2ed7efced28747aceaf3c412251296
XJINE <xjine.andorelseif@gmail.com>
2019-11-21 12:22:49 +0900

    Add SelectionElement.

---
+++ Assets/Packages/XGUI/SelectionElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XGUI
+{
+    public abstract class SelectionElement<T> : Component<T> where T : IList<T>
+    {
+        #region Property
+
+        protected Type Type { get; private set; }
+
+        #endregion Property
+
+        #region Constructor
+
+        public SelectionElement() : base() { }
+
+        public SelectionElement(string title) : base(title) { }
+
+        #endregion Constructor
+
+        #region Method
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            this.Type = typeof(T);
+        }
+
+        public abstract T Show(T value, IList<T> values);
+
+        #endregion Method
+    }
+}
---
+++ Assets/Packages/XGUI/Toolbar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace XGUI
+{
+    public class Toolbar<T> : SelectionElement<T> where T : IList<T>
+    {
+        #region Method
+
+        public override T Show(T value, IList<T> values)
+        {
+            base.ShowTitle();
+
+            int index = Mathf.Max(0, values.IndexOf(value));
+
+            if (this.Type == typeof(string))
+            {
+                index = GUILayout.Toolbar(index, values.Cast<string>().ToArray());
+            }
+            else if (this.Type == typeof(GUIContent))
+            {
+                index = GUILayout.Toolbar(index, values.Cast<GUIContent>().ToArray());
+            }
+            else if (this.Type == typeof(Texture))
+            {
+                index = GUILayout.Toolbar(index, values.Cast<Texture>().ToArray());
+            }
+            else
+            {
+                index = GUILayout.Toolbar(index, values.Cast<string>().ToArray());
+            }
+
+            return values[index];
+        }
+
+        #endregion Method
+    }
+}
