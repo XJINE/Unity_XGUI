9ba3f0b2b48e3624f296181b971270e70a6d5848
XJINE <xjine.andorelseif@gmail.com>
2022-05-22 08:20:07 +0900

    Make FoldoutPanel more convenient. Ex. enable to add other components into the button.

--- Assets/Packages/XGUI/Components/Panels/FoldoutPanel.cs
+++ Assets/Packages/XGUI/Components/Panels/FoldoutPanel.cs
@@ -5,6 +5,44 @@ namespace XGUI
 {
     public class FoldoutPanel : Panel<bool>
     {
+        #region Field
+
+        public Action ButtonFieldAction;
+
+        private   static GUIStyle _foldoutButtonStyle;
+        protected static GUIStyle FoldoutButtonStyle
+        {
+            get
+            {
+                _foldoutButtonStyle ??= new GUIStyle(GUI.skin.label)
+                {
+                    margin  = GUI.skin.button.margin,
+                    padding = GUI.skin.button.padding
+                };
+
+                return _foldoutButtonStyle;
+            }
+        }
+
+        private   static GUIStyle _foldoutBoxStyle;
+        protected static GUIStyle FoldoutBoxStyle
+        {
+            get
+            {
+                _foldoutBoxStyle ??= new GUIStyle(GUI.skin.label)
+                {
+                    padding = new RectOffset(GUI.skin.font.fontSize,
+                                             GUI.skin.box.padding.right,
+                                             GUI.skin.box.padding.top,
+                                             GUI.skin.box.padding.bottom)
+                };
+
+                return _foldoutBoxStyle;
+            }
+        }        
+
+        #endregion Field
+
         #region Constructor
 
         public FoldoutPanel() { }
@@ -23,7 +61,12 @@ namespace XGUI
             {
                 var buttonContent = (Value ? "\u25BC " : "\u25BA ") + Title;
 
-                Value = Value != GUILayout.Button(buttonContent, TitleStyle);
+                XGUILayout.HorizontalLayout(() =>
+                {
+                    var expand = !string.IsNullOrEmpty(Title);
+                    Value = Value != GUILayout.Button(buttonContent, FoldoutButtonStyle, GUILayout.ExpandWidth(expand));
+                    ButtonFieldAction?.Invoke();
+                });
 
                 if (Value)
                 {
@@ -33,7 +76,7 @@ namespace XGUI
                         {
                             action();
                         }
-                    });
+                    }, FoldoutBoxStyle);
                 }
             }, Value ? GUI.skin.box : null);
 
